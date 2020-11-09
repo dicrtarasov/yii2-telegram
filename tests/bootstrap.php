ea3b63187c274959fa3bf22820cd8d278ac39ead
Igor A Tarasov <develop@dicr.org>
2020-11-10 02:22:21 +0300

    ...

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license MIT
- * @version 05.11.20 05:02:40
+ * @version 10.11.20 02:21:50
  */
 
 /** @noinspection PhpUnhandledExceptionInspection */
@@ -22,17 +22,22 @@ new yii\console\Application([
     'id' => 'test',
     'basePath' => dirname(__DIR__),
     'components' => [
-        'cache' => yii\caching\FileCache::class,
+        'cache' => [
+            'class' => yii\caching\FileCache::class
+        ],
         'log' => [
+            'flushInterval' => 1,
             'targets' => [
                 [
                     'class' => yii\log\FileTarget::class,
-                    'levels' => ['error', 'warning', 'info', 'debug']
+                    'levels' => ['error', 'warning', 'info', 'trace']
                 ]
             ]
         ],
         'urlManager' => [
-            'hostInfo' => 'https://dicr.org'
+            'hostInfo' => 'https://dicr.org',
+            'baseUrl' => '',
+            'scriptUrl' => '/index.php'
         ]
     ],
     'modules' => [
@@ -40,5 +45,6 @@ new yii\console\Application([
             'class' => dicr\telegram\TelegramModule::class,
             'botToken' => 'ваш токен'
         ]
-    ]
+    ],
+    'bootstrap' => ['log']
 ]);
